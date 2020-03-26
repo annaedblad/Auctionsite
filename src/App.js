386704bb95796e1111cfbc365759ab543a7eb524
rilo84 <richard.losinno@gmail.com>
2020-03-26 15:14:07 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testar pusha
         </a>
       </header>
     </div>
