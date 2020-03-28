50087c9e71e479cf243b039a3f2ae3f23081fb62
Christian Eklund <christian.eklund123@gmail.com>
2020-03-28 16:58:52 +0100

    Navbar Bootstrap initiated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="headerContainer">
-        <h1>Header</h1>
+            <div className="jumbotron p-5 m-0">
+                <h1 className="text-center">Auction House 101</h1>
+            </div>
         </div>
     );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,12 +6,36 @@ const Navbar = () => {
     //Just for testing routing remove and replace with Bootstrap
     
     return (
-        <div className="navbarContainer">
-            <ul>
-                <li className="rightItems"><NavLink to="/">Auctions</NavLink></li>
-                <li className="rightItems"><NavLink to="/Details">Details</NavLink></li>
-                <li className="rightItems"><NavLink to="/Admin">Admin</NavLink></li>
-            </ul>
+        <div className="navbarContainer container">
+            <nav className="navbar navbar-expand-sm justify-content-center">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-link rightItems">
+                        <NavLink to="/">
+                            <span className="lead"><i class="fa fa-home"></i> Auctions</span>
+                        </NavLink>
+                    </li>
+                    <li className="nav-link rightItems">
+                        <NavLink to="/Details"> 
+                            <span className="lead">Details</span>
+                        </NavLink>
+                    </li>
+                    <li className="nav-link rightItems">
+                        <NavLink to="/Admin">
+                            <span className="lead">Admin</span>
+                        </NavLink>
+                    </li>
+                </ul>
+                <form className="form-inline">
+                    <input className="form-control mr-sm-2" 
+                        type="search" 
+                        placeholder="Serach for auction" 
+                        aria-label="Search" />
+                    <button className="btn btn-outline-info my-2 my-sm-0" 
+                        type="submit">
+                        Search
+                    </button>
+                </form>   
+            </nav>
         </div>
     );
 }
