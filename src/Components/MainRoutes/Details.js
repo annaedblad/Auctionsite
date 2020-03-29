cb1e44b316fb21935b9923346c755e16e27898a1
annaedblad <anna.edblad@hotmail.com>
2020-03-29 10:10:24 +0200

    added layout and css file for the Detailspage

--- src/Components/MainRoutes/Details.js
+++ src/Components/MainRoutes/Details.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
+import '../../Styling/Details.css';
+// import { Button } from 'reactstrap';
 
 const Details = () => {
     return (
-        <div className="detailsContainer">
-            <h1>Details</h1>
+        <div className="card" id="container">
+            <h1>Auktionstiteln</h1>
+            <Row>
+                <Col md={6} className="card" id="leftCard">
+                    <div className="paddingCards">
+                        <Row className="leftRow1">AUKTIONEN ÄR ÖPPEN/STÄNGD</Row>
+                        <Row className="leftRow2">Vinnande bud: </Row>
+                        <Row className="leftRow3">bild på objektet</Row>
+                        <Row className="leftRow4">Detaljerad beskrivning</Row>
+                    </div>
+                </Col>
+                <Col md={1} className="h"></Col>
+                <Col md={4} className="rightContainer card" id="rightCard">
+                    <div className="paddingCards">
+                        <Row>
+                            <Col md={12} id="utropsPris">Slutdatum <br></br> Utropspris <br></br> Antal bud</Col>
+                        </Row>
+                        <Row>
+                            <Col md={8} id="bud">Lista på bud</Col>
+
+                        </Row>
+                        <Row>
+                            <Col md={12}>
+                                <Form className="test">
+                                    <Form.Group controlId="formBasicPassword">
+                                        <Form.Control type="text" placeholder="Ange bud" />
+                                    </Form.Group>
+                                    <Form.Group controlId="formBasicCheckbox">
+                                    </Form.Group>
+                                    <Button variant="outline-dark" type="submit">
+                                        Lägg bud
+                                </Button>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 }
-
 export default Details;
+
