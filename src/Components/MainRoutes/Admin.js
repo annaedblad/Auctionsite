323100f2c19827eda74451b490bcdea3329fa91a
Sven Kalkbrenner <37480512+SveKal@users.noreply.github.com>
2020-03-29 16:03:40 +0200

    switched language to English

--- src/Components/MainRoutes/Admin.js
+++ src/Components/MainRoutes/Admin.js
@@ -14,26 +14,26 @@ const Admin = (/*props*/) => {
     return (
         <div className="card" id = "container">
             <blockquote class="blockquote text-center">
-                <h3>Admin Meny</h3>
+                <h3>Admin Menu</h3>
             </blockquote>            
             <div className = "row">
                 <div className = "col-sm">
                 <div className="card" id = "left">
                     <form onSubmit = {"some kind of logic"}>
                         <div className="form-group row">
-                            <label for="inputName" class="col-sm-2 col-form-label">Titel</label>
+                            <label for="inputName" class="col-sm-2 col-form-label">Title</label>
                             <div className="col-sm-9">
-                                <input type="text" className="form-control" id="name" required maxLength="50" minLength = "5" placeholder = "max 50 tecken"/>
+                                <input type="text" className="form-control" id="name" required maxLength="50" minLength = "5" placeholder = "max 50 signs"/>
                             </div>
                         </div>
                         <div className="form-group row">
-                            <label for="inputDescription" className="col-sm-2 col-form-label">Beskrivning</label>
+                            <label for="inputDescription" className="col-sm-2 col-form-label">Description</label>
                             <div className="col-sm-9">
-                                <textarea type="text" className="form-control" id="description" required maxLength="1000" minLength = "20" placeHolder = "max 1000 tecken"/>
+                                <textarea type="text" className="form-control" id="description" required maxLength="1000" minLength = "20" placeHolder = "max 1000 signs"/>
                             </div>
                         </div>
                         <div className="form-group row">
-                            <label for="inputPrice" className="col-sm-2 col-form-label">Pris</label>
+                            <label for="inputPrice" className="col-sm-2 col-form-label">Price</label>
                             <div className="col-sm-9">
                                 <input type="text" className="form-control" id="price" required/>
                             </div>
@@ -41,19 +41,19 @@ const Admin = (/*props*/) => {
                         <div className="form-group row">
                             <label className="col-sm-2 col-form-label">Start</label>
                             <div className="col-sm-9">
-                                <input type="text" className="form-control" id="start" placeholder = "Startar när du klickar på 'Lägg till'" disabled/>
+                                <input type="text" className="form-control" id="start" placeholder = "Starts when you click on 'Add new" disabled/>
                             </div>
                         </div>
                         <div className="form-group row">
-                            <label for="inputeEnd" className="col-2 col-form-label">Slut</label>
+                            <label for="inputeEnd" className="col-2 col-form-label">Expiration</label>
                             <div className="col-9">
                                 <input className="form-control" type="datetime-local" id="end" required/>
                             </div>
                         </div>
                         <div className="form-group row">                            
                             <div className="col-sm-11">                            
-                                <button type="submit" className="btn btn-outline-info my-2 my-sm-0 float-right">Lägg till / Uppdatera</button>  {/*needs to be toggable or 2 buttons*/}
-                                <button type="button" className="btn btn-outline-info my-2 my-sm-0 float-right">Rensa Formulär</button>                               
+                                <button type="submit" className="btn btn-outline-info my-2 my-sm-0 float-right">Add new / Update</button>  {/*needs to be toggable or 2 buttons*/}
+                                <button type="reset" className="btn btn-outline-info my-2 my-sm-0 float-right">Clear Form</button>                               
                             </div>                            
                         </div>
                     </form>
@@ -65,28 +65,28 @@ const Admin = (/*props*/) => {
                             <div class="card" id = "right">                                                        
                                 <ul className = "list-group overflow-auto">
                                     {/* just for testing, will be populated via mapping */ } 
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Ta bort</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
+                                    <a href="#" class="list-group-item list-group-item-action">Test 1 2 <button type="button" className="btn btn-outline-info btn-sm float-right">Delete</button></a>
                                 </ul>                             
                             </div>
                         </div>
