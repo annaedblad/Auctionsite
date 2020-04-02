834f5429fb3975fc63ce0d43a3ce4c92b3f3a5dd
Christian Eklund <christian.eklund123@gmail.com>
2020-04-02 09:19:25 +0200

    Autions.js uppdaterat

--- src/Components/MainRoutes/Auctions.js
+++ src/Components/MainRoutes/Auctions.js
@@ -2,7 +2,8 @@ import React, {useContext, useEffect} from 'react';
 import {AuctionContext} from '../../Contexts/AuctionContext';
 import {useParams} from 'react-router-dom';
 import {NavLink} from 'react-router-dom';
-
+import SoldAuction from '../../Styling/imgs/SoldAuction.jpg';
+import AuctionOpen from '../../Styling/imgs/AuctionOpen.png';
 const Auctions = () => {
   const { allAuctions, search, updateAllAuctions} = useContext(AuctionContext);
   let { id } = useParams();
@@ -14,15 +15,17 @@ const Auctions = () => {
   var date = new Date();
   let ongoingAuctions = allAuctions.filter(on => Date.parse(on.SlutDatum) > date.getTime());
 
-  if (id === "all") {
-    let actualAuctions = allAuctions
-        .filter(auction => auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search )
+  function getOngoing() {
+    return (
+        ongoingAuctions
+        .filter(auction => auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search)
         .map(auction => {
         return(
         <div className="col-sm-4" key={auction.AuktionID}>
         <div className="card">
             <NavLink className="link" to={`/Details/${auction.AuktionID}`}>
             <div id="auction_card">
+            <img src={AuctionOpen} id="imgSold" alt=""/>
                 <div className="card-body">
                     <h5 className="card-title text-sm-center">{auction.Titel}</h5>
                     <p className="card-text text-center">{auction.Beskrivning}</p>
@@ -39,50 +42,59 @@ const Auctions = () => {
         </div>
         );
     })
+    )
+  }
+
+  function getHistoric() {
+    return (
+        allAuctions.filter(auction => 
+        Date.parse(auction.SlutDatum) < date.getTime() && (auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search))
+            .map(auction => {
+              return (
+                <div className="col-sm-4" key={auction.AuktionID}>
+                  <div className="card">
+                    <a className="link" href={`/Details/${auction.AuktionID}`}>
+                    <div id="auction_card">
+                      <img src={SoldAuction} id="imgSold" alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title text-sm-center">{auction.Titel}</h5>
+                            <p className="card-text text-center">{auction.Beskrivning}</p>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">Start: {new Date(Date.parse(auction.StartDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
+                                <li className="list-group-item">Slut: {new Date(Date.parse(auction.SlutDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
+                                <li className="list-group-item">Utropspris: {auction.Utropspris} kr</li>
+                                <li className="list-group-item">Nuvarande pris: xx kr</li>
+                            </ul>
+                        </div>
+                    </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+    )}
+ 
+  if (id === "all") {
+
     return (
         <div className="auctionContainer container">
           <h1 className="text-center">All Auctions available here</h1>
           <div className="row justify-content-center">
-            {actualAuctions}
+            {getOngoing()}
+            <div className="row justify-content-center">
+            </div>
+            {getHistoric()}
           </div>
         </div>
       );
   }
   else if (id === "historic") {
-
-    let historicAuctions = allAuctions.filter(auction => 
-      Date.parse(auction.SlutDatum) < date.getTime() && (auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search))
-    .map(auction => {
-      return (
-        <div className="col-sm-4" key={auction.AuktionID}>
-          <div className="card">
-            <a className="link" href={`/Details/${auction.AuktionID}`}>
-            <div id="auction_card">
-                <div className="card-body">
-                    <h5 className="card-title text-sm-center">{auction.Titel}</h5>
-                    <p className="card-text text-center">{auction.Beskrivning}</p>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Start: {new Date(Date.parse(auction.StartDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
-                        <li className="list-group-item">Slut: {new Date(Date.parse(auction.SlutDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
-                        <li className="list-group-item">Utropspris: {auction.Utropspris} kr</li>
-                        <li className="list-group-item">Nuvarande pris: xx kr</li>
-                    </ul>
-                </div>
-            </div>
-            </a>
-          </div>
-        </div>
-      )
-    })
-
-    
     return (
-
       <div className="auctionContainer container text-center">
         <h1>Historical Auctions page</h1>
         <p className="lead">Detta är historiska sidan</p>
           <div className="row">
-            {historicAuctions}
+            {getHistoric()}
           </div>
 
       </div>
@@ -90,36 +102,11 @@ const Auctions = () => {
  
   } else {
 
-    let actualAuctions = ongoingAuctions
-        .filter(auction => auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search)
-        // .filter(on => Date.parse(on.SlutDatum) > date.getTime())
-        .map(auction => {
-        return(
-        <div className="col-sm-4" key={auction.AuktionID}>
-        <div className="card">
-            <NavLink className="link" to={`/Details/${auction.AuktionID}`}>
-            <div id="auction_card">
-                <div className="card-body">
-                    <h5 className="card-title text-sm-center">{auction.Titel}</h5>
-                    <p className="card-text text-center">{auction.Beskrivning}</p>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Start: {new Date(Date.parse(auction.StartDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
-                        <li className="list-group-item">Slut: {new Date(Date.parse(auction.SlutDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
-                        <li className="list-group-item">Utropspris: {auction.Utropspris} kr</li>
-                        <li className="list-group-item">Nuvarande pris: xx kr</li>
-                    </ul>
-                </div>
-            </div>
-            </NavLink>
-        </div>
-        </div>
-        );
-    })
     return (
         <div className="auctionContainer container">
           <h1 className="text-center">Actual Auctions page</h1>
           <div className="row justify-content-center">
-            {actualAuctions}
+            {getOngoing()}
           </div>
         </div>
       );
