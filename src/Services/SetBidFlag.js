aa45dcd6d31624c961da721759676374100b90e5
rilo84 <richard.losinno@gmail.com>
2020-04-02 20:58:25 +0200

    minor fix

--- src/Services/SetBidFlag.js
+++ src/Services/SetBidFlag.js
@@ -1,7 +1,7 @@
 import GetAllBids from "../Repo/GetAllBids";
 
-const setBidFlag = async allAuctions => {
-  allAuctions.forEach(auction => {
+const SetBidFlag = async allAuctions => {
+  await allAuctions.forEach(auction => {
     GetAllBids(auction.AuktionID).then(bids => {
       if (bids.length > 0) auction.hasBid = true;
       else auction.hasBid = false;
@@ -9,4 +9,4 @@ const setBidFlag = async allAuctions => {
   });
 };
 
-export default setBidFlag;
+export default SetBidFlag;
