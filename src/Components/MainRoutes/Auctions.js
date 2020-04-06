3e6bbb4ba1a7e14903ca970e1b5bf933764138a2
Christian Eklund <christian.eklund123@gmail.com>
2020-04-06 10:47:20 +0200

    SökFunktion uppdaterad

--- src/Components/MainRoutes/Auctions.js
+++ src/Components/MainRoutes/Auctions.js
@@ -15,11 +15,12 @@ const Auctions = () => {
 
   var date = new Date();
   let ongoingAuctions = allAuctions.filter(on => Date.parse(on.SlutDatum) > date.getTime());
+  let historicAuctions = allAuctions.filter(on => Date.parse(on.SlutDatum) < date.getTime());
 
   function getOngoing() {
     return (
         ongoingAuctions
-        .filter(auction => auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search)
+        .filter(auction => auction)
         .map(auction => {
         return(
         <div className="col-sm-4" key={auction.AuktionID}>
@@ -44,12 +45,11 @@ const Auctions = () => {
     })
     )
   }
-
+  // && (auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search))
   function getHistoric() {
     return (
-        allAuctions.filter(auction => 
-        Date.parse(auction.SlutDatum) < date.getTime() && (auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search))
-            .map(auction => {
+        historicAuctions.filter(auction => auction)
+              .map(auction => {
               return (
                 <div className="col-sm-4" key={auction.AuktionID}>
                   <div className="card historicCard">
@@ -73,16 +73,71 @@ const Auctions = () => {
             })
     )}
  
+
+    function getOngoingWithQuery() {
+      return (
+        ongoingAuctions.filter(auction => auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search)
+          .map(auction => {
+          return(
+          <div className="col-sm-4" key={auction.AuktionID}>
+          <div className="card activeCard">
+              <NavLink className="link" to={`/Details/${auction.AuktionID}`}>
+              <div id="auction_card">
+              <img src={AuctionOpen} id="imgSold" alt=""/>
+                  <div className="card-body">
+                      <h5 className="card-title text-sm-center">{auction.Titel}</h5>
+                      <p className="card-text text-center">{auction.Beskrivning}</p>
+                      <ul className="list-group list-group-flush">
+                          <li className="list-group-item active-item">Start: {new Date(Date.parse(auction.StartDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
+                          <li className="list-group-item active-item">Slut: {new Date(Date.parse(auction.SlutDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
+                          <li className="list-group-item active-item">Utropspris: {auction.Utropspris} kr</li>
+                      </ul>
+                  </div>
+              </div>
+              </NavLink>
+          </div>
+          </div>
+          );
+      })
+      )
+    
+  }
+  function getHistoricWithQuery() {
+    return (
+        historicAuctions.filter(auction => auction.Titel.toLowerCase().includes(search.toLowerCase()) || !search)
+          .map(auction => {
+            return (
+              <div className="col-sm-4" key={auction.AuktionID}>
+                <div className="card historicCard">
+                  <a className="link" href={`/Details/${auction.AuktionID}`}>
+                  <div id="auction_card">
+                    <img src={SoldAuction} id="imgSold" alt=""/>
+                      <div className="card-body">
+                          <h5 className="card-title text-sm-center">{auction.Titel}</h5>
+                          <p className="card-text text-center">{auction.Beskrivning}</p>
+                          <ul className="list-group list-group-flush">
+                              <li className="list-group-item historic-item">Start: {new Date(Date.parse(auction.StartDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
+                              <li className="list-group-item historic-item">Slut: {new Date(Date.parse(auction.SlutDatum)).toLocaleString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).replace(',','')}</li>
+                              <li className="list-group-item historic-item">Utropspris: {auction.Utropspris} kr</li>
+                          </ul>
+                      </div>
+                  </div>
+                  </a>
+                </div>
+              </div>
+            )
+          })
+    )}
+ 
+
   if (id === "all") {
 
     return (
         <div className="auctionContainer container">
-          <h1 className="text-center">Sökresultat</h1>
+          <h1 className="text-center">Alla auktioner</h1>
           <div className="row justify-content-center">
-            {getOngoing()}
-            <div className="row justify-content-center">
-            </div>
-            {getHistoric()}
+            {getOngoingWithQuery()}
+            {getHistoricWithQuery()}
           </div>
         </div>
       );
