d9b2cd46fc2491786c31369596fd756b629532a2
annaedblad <anna.edblad@hotmail.com>
2020-04-06 09:00:52 +0200

    cleaned up some code in details

--- src/Components/MainRoutes/Details.js
+++ src/Components/MainRoutes/Details.js
@@ -1,13 +1,12 @@
 import React, { useContext, useState } from "react";
-import { Row, Col, Form} from 'react-bootstrap';
-// import { Form } from 'react-bootstrap/Form';
+import { Row, Col, Form } from 'react-bootstrap';
 import '../../Styling/Details.css';
 import { useParams } from "react-router-dom";
 import { AuctionContext } from "../../Contexts/AuctionContext";
 var tempoId = "";
 
 const Details = () => {
-    
+
     let { id } = useParams();
     const { allAuctions, getBids, bids, setNewBid } = useContext(AuctionContext);
     const specificAuction = allAuctions.find(x => x.AuktionID == id);
@@ -62,8 +61,8 @@ const Details = () => {
         let text = "";
         let winningBid = "";
         var d = new Date(specificAuction.SlutDatum);
-        var endDateString = d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2)  + " " +
-        ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+        var endDateString = d.getFullYear() + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) + " " +
+            ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
 
         if (isOpen === true) {
             text = "Auktionen är öppen";
@@ -77,140 +76,88 @@ const Details = () => {
             listBids = "Budhistorik ej tillgänglig vid avslutad auktion";
         }
 
-    return (
-        <div className="container">
-           <h1 className="text-center" id="auctionTitle">Auktionsobjekt : {specificAuction.Titel}</h1>
+        return (
+            <div className="container">
+                <h1 className="text-center" id="auctionTitle">Auktionsobjekt : {specificAuction.Titel}</h1>
 
-            <div className="row mainRow">
-                {/* Här börjar Left Container */}
-                <div className="col-md-6">
-                    <Row className="alert status-row"> 
-                        <Col>
-                             <span className="lead font-weight-bold">{text}</span>
-                        </Col>
-                    </Row>
-
-                    <Row className="alert seller-row">
-                        <Col md={6}>
-                            <h5 className="text-right">Försäljare =></h5>
-                        </Col>
-                        <Col md={6}>
-                            <h5 className="text-left">{specificAuction.SkapadAv}</h5>
-                        </Col>
-                    </Row>
-                    <Row className="alert winner-row">
-                        <Col md={12}><h5 className="text-center">{winningBid}</h5></Col>
-                    </Row>
-                        
-                     <Row className="text-center alert description-row">
-                        <Col md={12}> <h4> Beskrivning </h4> <h5>{specificAuction.Beskrivning}</h5> </Col>
-                     </Row>
-                </div>
-
-
-                {/* Här börjar right container */}
-                <div className="col-md-5" id="rightDiv">
-                    <Row className="headrow">
-                        <Col md={12}> <h5>Slutdatum: {endDateString}</h5></Col>
-                    </Row>
-                    <Row className="contentrow">
-                        <Col md={12} className="price-col"><h5 className="price-text">Utropspris 
-                                <span className="alert price-box">{specificAuction.Utropspris} kr</span>
-                            </h5>
-                        </Col>
-                        <Col md={12} className="bid-col"> 
-                           <h5 className="bid-text"> Antal bud:
-                           <span className="alert amount-bid-box">{items.length}</span>
-                           </h5> 
-                        </Col>
-                        <Row>
-                            <Col md={12}> <h5 className="text-center mt-5">Lista på bud</h5></Col>
-                            <Col md={10} className="text-center" id="bud"> 
-                                <div>
-                                    {listBids}
-                                </div>
+                <div className="row mainRow">
+                    {/* Här börjar Left Container */}
+                    <div className="col-md-6">
+                        <Row className="alert status-row">
+                            <Col>
+                                <span className="lead font-weight-bold">{text}</span>
                             </Col>
                         </Row>
-                        <Row>
-                            <div className="container-fluid justify-content-center">
-                                <Col md={12} className="mt-5">
-                                    <Form className="test" onSubmit={handleSubmit}>
-                                        <Form.Group controlId="formName">
-                                            <Form.Control type="text" name="formName" placeholder="Ange namn" required minLength="2" disabled={!isOpen} />
-                                        </Form.Group>
-                                        <Form.Group controlId="formBid">
-                                            <Form.Control type="number" name="bidAmount" placeholder="Ange bud" required minLength="1" disabled={!isOpen} />
-                                        </Form.Group>
-                                        <button id="bidButton" className="btn btn-outline-warning" type="submit" disabled={!isOpen}>
-                                            Lägg bud
-                                        </button>
-                                        <div className="errorMessage">{error}</div>
-                                    </Form>
+
+                        <Row className="alert seller-row">
+                            <Col md={6}>
+                                <h5 className="text-right">Försäljare =></h5>
+                            </Col>
+                            <Col md={6}>
+                                <h5 className="text-left">{specificAuction.SkapadAv}</h5>
+                            </Col>
+                        </Row>
+                        <Row className="alert winner-row">
+                            <Col md={12}><h5 className="text-center">{winningBid}</h5></Col>
+                        </Row>
+
+                        <Row className="text-center alert description-row">
+                            <Col md={12}> <h4> Beskrivning </h4> <h5>{specificAuction.Beskrivning}</h5> </Col>
+                        </Row>
+                    </div>
+
+
+                    {/* Här börjar right container */}
+                    <div className="col-md-5" id="rightDiv">
+                        <Row className="headrow">
+                            <Col md={12}> <h5>Slutdatum: {endDateString}</h5></Col>
+                        </Row>
+                        <Row className="contentrow">
+                            <Col md={12} className="price-col"><h5 className="price-text">Utropspris
+                                <span className="alert price-box">{specificAuction.Utropspris} kr</span>
+                            </h5>
+                            </Col>
+                            <Col md={12} className="bid-col">
+                                <h5 className="bid-text"> Antal bud:
+                           <span className="alert amount-bid-box">{items.length}</span>
+                                </h5>
+                            </Col>
+                            <Row>
+                                <Col md={12}> <h5 className="text-center mt-5">Lista på bud</h5></Col>
+                                <Col md={10} className="text-center" id="bud">
+                                    <div>
+                                        {listBids}
+                                    </div>
                                 </Col>
-                            </div>
+                            </Row>
+                            <Row>
+                                <div className="container-fluid justify-content-center">
+                                    <Col md={12} className="mt-5">
+                                        <Form className="test" onSubmit={handleSubmit}>
+                                            <Form.Group controlId="formName">
+                                                <Form.Control type="text" name="formName" placeholder="Ange namn" required minLength="2" disabled={!isOpen} />
+                                            </Form.Group>
+                                            <Form.Group controlId="formBid">
+                                                <Form.Control type="number" name="bidAmount" placeholder="Ange bud" required minLength="1" disabled={!isOpen} />
+                                            </Form.Group>
+                                            <button id="bidButton" className="btn btn-outline-warning" type="submit" disabled={!isOpen}>
+                                                Lägg bud
+                                        </button>
+                                            <div className="errorMessage">{error}</div>
+                                        </Form>
+                                    </Col>
+                                </div>
+                            </Row>
+
+
                         </Row>
 
 
-                     </Row>
-
-
+                    </div>
                 </div>
             </div>
-        </div>
-    
-    // <div className="container-fluid">
-    //     <div className="card" id="container">
-    //         <h1 className="text-center">{specificAuction.Titel}</h1>
-    //             <Row className="text-center">
-    //                 <Col md={6} className="card" id="leftCard">
-    //                     <div className="alert alert-dark"> 
-    //                         <span className="lead font-weight-bold">{text}</span>
-    //                     </div>
-    //                 <Col><label className="lead text-center">Försäljare</label></Col>
 
-    //                     <Col className="leftRow1">{specificAuction.SkapadAv}</Col>
-    //                     <Row className="leftRow2">{winningBid}</Row>
-    //                         <label className="lead">Beskrivning</label>
-    //                     <Row className="leftRow4">{specificAuction.Beskrivning}</Row>
-                          
-                        
-    //                 </Col>
-    //                 <Col md={1} className="h"></Col>
-
-    //                 <Col md={4} className="rightContainer card" id="rightCard">
-    //                     <div className="paddingCards">
-    //                         <Row>
-    //                             <Col md={12} id="utropsPris">Slutdatum: {endDateString} <br></br> 
-    //                             Utropspris: {specificAuction.Utropspris} kr <br></br> 
-    //                             Antal bud: {items.length}</Col>
-    //                         </Row>
-    //                         <Row>
-    //                             <Col md={8} id="bud"> Lista på bud: {listBids}
-    //                             </Col>
-
-    //                         </Row>
-    //                         <Row>
-    //                             <Col md={12}>
-    //                                 <Form className="test" onSubmit={handleSubmit}>
-    //                                     <Form.Group controlId="formName">
-    //                                         <Form.Control type="text" name="formName" placeholder="Ange namn" required minLength="2" disabled={!isOpen} />
-    //                                     </Form.Group>
-    //                                     <Form.Group controlId="formBid">
-    //                                         <Form.Control type="text" name="bidAmount" placeholder="Ange bud" required minLength="1" disabled={!isOpen} />
-    //                                     </Form.Group>
-    //                                     <Button id="bidButton" className="btn btn-secondary" type="submit" disabled={!isOpen}>
-    //                                         Lägg bud
-    //                                  </Button>
-    //                                     <div className="errorMessage">{error}</div>
-    //                                 </Form>
-    //                             </Col>
-    //                         </Row>
-    //                     </div>
-    //                 </Col>
-    //             </Row>
-    //         </div>
-    //     </div>
-        ); 
+        );
     }
     else {
         getBids(id);
